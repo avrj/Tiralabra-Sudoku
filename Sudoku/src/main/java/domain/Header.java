9384288c49b38f6fc8fd97e100dac97f84e50739
avrj <anraiha@cs.helsinki.fi>
2015-11-05 12:07:55 +0200

    added header class which defines the headers in exact cover problem

---
+++ Sudoku/src/main/java/domain/Header.java
@@ -0,0 +1,65 @@
+package domain;
+
+/**
+ *
+ */
+public class Header extends Node {
+    /**
+     *
+     */
+    private int size;
+
+    /**
+     *
+     */
+    private int name;
+
+    /**
+     *
+     */
+    public Header() {
+        this(0);
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public Header(int name) {
+        this.name = name;
+        this.size = 0;
+        this.setHeader(this);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     *
+     * @param size
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
