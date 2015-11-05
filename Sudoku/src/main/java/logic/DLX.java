ae4d5f391f0e7942993c0aeb0f488f35fdc846dd
avrj <anraiha@cs.helsinki.fi>
2015-11-05 13:23:10 +0200

    added dlx uncovering method

--- Sudoku/src/main/java/logic/DLX.java
+++ Sudoku/src/main/java/logic/DLX.java
@@ -8,7 +8,7 @@ import domain.Node;
  */
 public class DLX {
     /**
-     * Covers one column by header using DLX cover method
+     * Covers one column/removes constraint by header using DLX cover method
      *
      * @param headerColumn Header to cover
      */
@@ -31,5 +31,31 @@ public class DLX {
 
             node1 = node1.getDown();
         }
+    }
+
+    /**
+     * Uncovers one column/reverse constraint by header using DLX uncover method
+     *
+     * @param headerColumn Header to uncover
+     */
+    public void uncoverColumn(Header headerColumn) {
+        Node node1 = headerColumn.getUp();
+        Node node2 = null;
+
+        while(!node1.equals(headerColumn)) {
+            node2 = node1.getLeft();
+
+            while(!node1.equals(node2)) {
+                node2.getUp().setDown(node2);
+                node2.getDown().setUp(node2);
+                node2.getHeader().setSize(node2.getHeader().getSize() + 1);
+                node2 = node2.getLeft();
+            }
+
+            node1 = node1.getUp();
+        }
+
+        node2.getLeft().setRight(node2);
+        node2.getRight().setLeft(node2);
     }
 }
